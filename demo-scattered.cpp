7c854f8079c71e319b6e3c55893d15ec5d10b65a
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-06-02 01:17:10 +0200

    Add an example that allocates data in a scattered way

---
+++ demo-scattered.cpp
@@ -0,0 +1,27 @@
+#include "gc.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main() {
+  GC_INIT();
+
+  for (int i = 0; i < 10000; ++i) {
+    int size = i % 100;
+
+    int *p = (int*)GC_MALLOC_UNCOLLECTABLE(sizeof(int) * size);
+
+    printf("Step %d - Allocated: %d - At: %x\n", i, size, p);
+
+    for (int j = 0; j < size; j++) {
+      assert(p[j] == 0);
+      p[j] = j;
+      assert(p[j] == j);
+    }
+
+    if (i % 1000 == 0) {
+      printf("Heap size = %d\n", GC_get_heap_size());
+    }
+  }
+
+  return 0;
+}
