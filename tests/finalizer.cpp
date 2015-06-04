80ecf7bcbfc79846f669dcec9b609abbe646f920
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-06-04 22:52:30 +0200

    Add tests of allocation with explicit type information

--- tests/finalizer.cpp
+++ tests/finalizer.cpp
@@ -1,5 +1,6 @@
 #include "gc.h"
 #include "gc_backptr.h"
+#include "gc_typed.h"
 #include <assert.h>
 #include <stdio.h>
 
@@ -18,7 +19,7 @@ void GC_CALLBACK finalizerLinked(void* obj, void* client_data) {
 void testFinalizer1() {
   finalizerCalled = false;
 
-  int *p = (int*)GC_MALLOC(sizeof(int));
+  int* p = (int*)GC_MALLOC(sizeof(int));
   assert(*p == 0);
   *p = 5;
   assert(*p == 5);
@@ -34,7 +35,7 @@ void testFinalizer1() {
 void testFinalizer2() {
   finalizerCalled = false;
 
-  int *p = (int*)GC_MALLOC_UNCOLLECTABLE(sizeof(int));
+  int* p = (int*)GC_MALLOC_UNCOLLECTABLE(sizeof(int));
   assert(*p == 0);
   *p = 5;
   assert(*p == 5);
@@ -56,9 +57,9 @@ void testFinalizer3() {
   finalizerCalled = false;
   finalizerLinkedCalled = false;
 
-  int **p = (int**)GC_MALLOC(sizeof(int*));
+  int** p = (int**)GC_MALLOC(sizeof(int*));
   assert(*p == 0);
-  int *q = (int*)GC_MALLOC(sizeof(int));
+  int* q = (int*)GC_MALLOC(sizeof(int));
   assert(*q == 0);
 
   *p = q;
@@ -82,9 +83,9 @@ void testFinalizer4() {
   finalizerCalled = false;
   finalizerLinkedCalled = false;
 
-  int **p = (int**)GC_MALLOC_UNCOLLECTABLE(sizeof(int*));
+  int** p = (int**)GC_MALLOC_UNCOLLECTABLE(sizeof(int*));
   assert(*p == 0);
-  int *q = (int*)GC_MALLOC(sizeof(int));
+  int* q = (int*)GC_MALLOC(sizeof(int));
   assert(*q == 0);
 
   *p = q;
@@ -118,9 +119,9 @@ void testFinalizer5() {
   finalizerCalled = false;
   finalizerLinkedCalled = false;
 
-  int **p = (int**)GC_MALLOC(sizeof(int*));
+  int** p = (int**)GC_MALLOC(sizeof(int*));
   assert(*p == 0);
-  int *q = (int*)GC_MALLOC_UNCOLLECTABLE(sizeof(int));
+  int* q = (int*)GC_MALLOC_UNCOLLECTABLE(sizeof(int));
   assert(*q == 0);
 
   *p = q;
@@ -144,6 +145,96 @@ void testFinalizer5() {
   GC_FREE(q);
 }
 
+struct obj {
+  int* pointer1;
+  int* value1;
+  int* pointer2;
+};
+
+bool finalizerObjCalled;
+bool finalizerPointer1Called;
+bool finalizerValue1Called;
+bool finalizerPointer2Called;
+
+void GC_CALLBACK finalizerObj(void* obj, void* client_data) {
+  finalizerObjCalled = true;
+}
+
+void GC_CALLBACK finalizerPointer1(void* obj, void* client_data) {
+  finalizerPointer1Called = true;
+}
+
+void GC_CALLBACK finalizerValue1(void* obj, void* client_data) {
+  finalizerValue1Called = true;
+}
+
+void GC_CALLBACK finalizerPointer2(void* obj, void* client_data) {
+  finalizerPointer2Called = true;
+}
+
+void testFinalizer6(bool value1pointer) {
+  finalizerObjCalled = false;
+  finalizerPointer1Called = false;
+  finalizerValue1Called = false;
+  finalizerPointer2Called = false;
+
+  GC_descr obj_descr;
+  GC_word obj_bitmap[GC_BITMAP_SIZE(obj)] = { 0 };
+  GC_set_bit(obj_bitmap, GC_WORD_OFFSET(obj, pointer1));
+  if (value1pointer) {
+    GC_set_bit(obj_bitmap, GC_WORD_OFFSET(obj, value1));
+  }
+  GC_set_bit(obj_bitmap, GC_WORD_OFFSET(obj, pointer2));
+
+  obj_descr = GC_make_descriptor(obj_bitmap, GC_WORD_LEN(obj));
+
+  obj** ptr_to_an_obj = (obj**)GC_MALLOC_UNCOLLECTABLE(sizeof(obj**));
+
+  obj* an_obj = (obj*)GC_MALLOC_EXPLICITLY_TYPED(sizeof(obj), obj_descr);
+
+  *ptr_to_an_obj = an_obj;
+
+  assert(an_obj->pointer1 == 0);
+  assert(an_obj->value1 == 0);
+  assert(an_obj->pointer2 == 0);
+
+  int* pointer1 = (int*)GC_MALLOC(sizeof(int));
+  int* value1 = (int*)GC_MALLOC(sizeof(int));
+  int* pointer2 = (int*)GC_MALLOC(sizeof(int));
+
+  GC_REGISTER_FINALIZER((void*)an_obj, finalizerObj, NULL, (GC_finalization_proc*)0, (void**)0);
+  GC_REGISTER_FINALIZER((void*)pointer1, finalizerPointer1, NULL, (GC_finalization_proc*)0, (void**)0);
+  GC_REGISTER_FINALIZER((void*)value1, finalizerValue1, NULL, (GC_finalization_proc*)0, (void**)0);
+  GC_REGISTER_FINALIZER((void*)pointer2, finalizerPointer2, NULL, (GC_finalization_proc*)0, (void**)0);
+
+  an_obj->pointer1 = pointer1;
+  an_obj->value1 = value1;
+  an_obj->pointer2 = pointer2;
+
+  GC_gcollect();
+
+  assert(!finalizerObjCalled);
+  assert(!finalizerPointer1Called);
+  if (value1pointer) {
+    assert(!finalizerValue1Called);
+  } else {
+    assert(finalizerValue1Called);
+  }
+  assert(!finalizerPointer2Called);
+
+  GC_FREE(ptr_to_an_obj);
+
+  GC_gcollect();
+
+  assert(finalizerObjCalled);
+
+  GC_gcollect();
+
+  assert(finalizerPointer1Called);
+  assert(finalizerValue1Called);
+  assert(finalizerPointer2Called);
+}
+
 int main() {
   GC_INIT();
 
@@ -152,6 +243,8 @@ int main() {
   testFinalizer3();
   testFinalizer4();
   testFinalizer5();
+  testFinalizer6(true);
+  testFinalizer6(false);
 
   return 0;
 }
